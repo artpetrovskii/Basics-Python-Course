285b8aa56c2770cf53677b634d0f946468bee99d
artpetrovskii <46283509+artpetrovskii@users.noreply.github.com>
2020-07-05 23:35:49 +0300

    Update lesson 4. exercise 3.py

--- lesson 4. exercise 3.py
+++ lesson 4. exercise 3.py
@@ -8,10 +8,10 @@ def attack(person1, person2):
         return person1['health'], person2
     my_wanzer = {
         'health': 100, 'damage': 30, 'name': input('Введите имя Ванзера')
-        }
+    }
     OCU = {
         'health': 140, 'damage': 20, 'name': input('Введите противника Объединенного Океанического Союза')
-        }
+    }
     turn = 1
     while my_wanzer['health'] <= 0 and OCU['health'] <= 0:
         if turn % 2 != 0:
