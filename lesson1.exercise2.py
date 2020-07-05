0a68ee10d48365545b88a236821d288e691f145b
artpetrovskii <46283509+artpetrovskii@users.noreply.github.com>
2020-07-06 00:45:57 +0300

    Delete lesson1.exercise2.py

--- lesson1.exercise2.py
+++
@@ -1,8 +0,0 @@
-number = int(input())
-
-while number not in range(0, 10):
-    print('Неверный ввод. Число должно быть [0, 10). Попробуйте снова')
-    number = int(input())
-
-else:
-    print(number ** 2)
