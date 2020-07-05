0133eb71b95c4c6a3276139c2f9be1b2b3893e14
artpetrovskii <46283509+artpetrovskii@users.noreply.github.com>
2020-07-05 23:32:56 +0300

    Update my_random.py

--- my_random.py
+++ my_random.py
@@ -1,10 +1,12 @@
 import random
 
+
 def get_random_element(lst):
     if lst:
         return random.choice(lst)
 
+
 print('"my_random.py" imported ')
 if __name__ == '__main__':
     test_list = [0, 1, 2, 3, 4, 5]
-print(get_random_element(test_list))
+    print(get_random_element(test_list))
