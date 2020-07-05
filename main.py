afa264adc1f6ebfdcecec981d2e4741fb66e80cb
artpetrovskii <46283509+artpetrovskii@users.noreply.github.com>
2020-07-05 23:31:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import my_random
 
 my_folders.create_forlders(9)
 my_folders.delete_forlders(9)
-print(f'random element from non-empty list: {my_random.get_random_element([1, 2, 3, 4])}')
-print(f'random element from empty list: {my_random.get_random_element([])}')
+print(f'random element from non-empty list:{my_random.get_random_element([1,2,3,4])}')
+print(f'random element from empty list:{my_random.get_random_element([])}')
