4df3459175ff5c3bee81ba6a647fe55dbc760a0d
artpetrovskii <46283509+artpetrovskii@users.noreply.github.com>
2020-07-05 23:32:36 +0300

    Update my_folders.py

--- my_folders.py
+++ my_folders.py
@@ -1,18 +1,21 @@
 import os
 
+
 def create_forlders(n):
     for i in range(1, n+1):
         folder_name = 'folder_{:02}'.format(i)
         os.mkdir(folder_name)
         print(f'created folder: {folder_name}')
-print('donen\n')
+    print('donen\n')
+
 
 def delete_forlders(n):
     for i in range(1, n+1):
         folder_name = 'folder_{:02}'.format(i)
         os.rmdir(folder_name)
         print(f'deleted folder: {folder_name}')
-print('done\n')
+    print('done\n')
+
 
 print('"my_folders.py" imported ')
 if __name__ == '__main__':
