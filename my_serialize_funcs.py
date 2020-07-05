61ad209fd14d954fb5875d46364f43bef94fa901
artpetrovskii <46283509+artpetrovskii@users.noreply.github.com>
2020-07-05 20:13:02 +0300

    Update my_serialize_funcs.py

--- my_serialize_funcs.py
+++ my_serialize_funcs.py
@@ -2,19 +2,15 @@ import os
 import pickle
 import json
 
-
 def get_json_from_data(data):
     return json.dumps(data)
-
 
 def get_data_from_json(json_):
     return json.loads(json_)
 
-
 def put_json_data_to_file(data, filename, encoding='utf-8'):
     with open(filename, 'w', encoding=encoding) as out_f:
         json.dump(data, out_f)
-
 
 def get_json_data_from_file(filename):
     if os.path.exists(filename):
@@ -24,19 +20,15 @@ def get_json_data_from_file(filename):
         info = f'file {filename} not exists!'
         return info
 
-
 def get_pickle_from_data(data):
     return pickle.dumps(data)
-
 
 def get_data_from_pickle(pickle_):
     return pickle.loads(pickle_)
 
-
 def put_pickle_data_to_file(data, filename):
     with open(filename, 'wb') as out_f:
         pickle.dump(data, out_f)
-
 
 def get_pickle_data_from_file(filename):
     if os.path.exists(filename):
