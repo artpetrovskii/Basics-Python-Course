85b1537631504e4fbc6268739a84c4a8292cb121
Artemii <artemii.petrovskii@outlook.com>
2020-07-06 13:59:46 +0300

    lesson 2. exercise 1 modified

--- lesson 2. exercise 1.py
+++ lesson 2. exercise 1.py
@@ -1,7 +1,4 @@
 list1 = ['4', 'dfsdf', 'kh43', 'vlof', 'dfsdf']
 list2 = ['456', 'dfsdf', '89']
 
-for item in list2:
-    if item in list1:
-        list1.remove(item)
-print(list1)
+print([k for k in list1 if list2.count(k) < 1])
